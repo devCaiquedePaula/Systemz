688edf255e283b846e35fd7bdf7590870f8102dc
DevCaique <dev.caiquedepaula@gmail.com>
2026-05-06 00:06:44 -0300

    Setup inicial

---
+++ src/main/java/dev/kiq/systemz/SystemzApplication.java
@@ -0,0 +1,13 @@
+package dev.kiq.systemz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kiq/systemz/SystemzApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kiq.systemz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
